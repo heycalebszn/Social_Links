327090b292f274c3090712bfa9ff329a11aacb0c
caleb <kalejaiyecaleb@gmail.com>
2024-06-20 11:57:02 +0100

    commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import SocialLinks from "./components/SocialLinks";
+
+const App = () => {
+  return (
+    <div className="">
+      <SocialLinks />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/assets/index.js
@@ -0,0 +1,4 @@
+import avatar from "./avatar-jessica.jpeg";
+import favicon from "./favicon-32x32.png";
+
+export { avatar, favicon };
