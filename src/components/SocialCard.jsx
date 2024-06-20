89e15ab19b85341d11c9566a550377bd0f783b78
caleb <kalejaiyecaleb@gmail.com>
2024-06-20 16:35:29 +0100

    commit

--- src/components/SocialCard.jsx
+++ src/components/SocialCard.jsx
@@ -4,7 +4,7 @@ import { avatar } from "../assets";
 const SocialLinks = () => {
   return (
     <div className="items-center justify-center m-auto text-center cardBackground w-fit h-fit px-[10px] py-[10px]">
-      <img className="m-auto rounded-full" src={avatar} alt="" />{" "}
+      <img className="m-auto rounded-full w-[50px]" src={avatar} alt="" />{" "}
     </div>
   );
 };
